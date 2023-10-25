c8247bd11efb8bfb871a46198e894707c604f4c8
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-10-25 17:45:44 +0100

    done create-post ui

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -6,8 +6,8 @@ const FormField = ({
   name,
   placeholder,
   value,
-  isSurpriseMe,
   handleChange,
+  isSurpriseMe,
   handleSurpriseMe,
 }) => {
   return (
@@ -23,13 +23,22 @@ const FormField = ({
           <button
             type='button'
             onClick={handleSurpriseMe}
-            className='font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black'
+            className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
           >
-            {" "}
-            Surprise Me
+            Surprise me
           </button>
         )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+      />
     </div>
   );
 };
