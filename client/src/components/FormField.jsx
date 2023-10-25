fef8b3ab425871d9572921d9083142ccf7cccc2e
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-10-25 16:40:48 +0100

    done createpost ui

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  isSurpriseMe,
+  handleChange,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={name}
+          className='block text-sm font-medium text-gray-900'
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black'
+          >
+            {" "}
+            Surprise Me
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FormField;
